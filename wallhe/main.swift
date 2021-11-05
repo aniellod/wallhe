4882a9254fba383b2b4c4fec4aa354bb5743b402
Aniello <aniellod@hotmail.com>
2021-11-05 16:02:36 -0400

    first commit

--- wallhe/main.swift
+++ wallhe/main.swift
@@ -12,9 +12,38 @@ import CoreGraphics
 func setBackground(theURL: String) {
     let workspace = NSWorkspace()
     let fixedURL = URL(string: theURL)
+    //let imageScaling: NSWorkspace.DesktopImageOptionKey
+    var options = [NSWorkspace.DesktopImageOptionKey: Any]()
+    options[.imageScaling] = NSImageScaling.scaleProportionallyUpOrDown.rawValue
+    options[.allowClipping] = false
     do {
-        try workspace.setDesktopImageURL(fixedURL!, for: NSScreen.screens[1])
+//        try workspace.setDesktopImageURL(fixedURL!, for: NSScreen.screens[1])
+        try workspace.setDesktopImageURL(fixedURL!, for: NSScreen.screens[1], options: options)
     } catch {print("duh")}
+}
+
+func buildWallpaper(sample: NSImage, sample2: NSImage, desktopSize: NSSize) -> NSImage {
+    //  Converted to Swift 5.5 by Swiftify v5.5.22755 - https://swiftify.com/
+    let resultImage = NSImage(size: (NSMakeSize(1920,1080)))
+    resultImage.lockFocus()
+    //let sample2 = sample
+    //var anotherImage = NSImage(contentsOf: sample1)
+    sample.draw(at: NSPoint(x: 0, y: 0), from: NSRect.zero, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
+   
+    sample.draw(at: NSPoint(x: sample.size.width, y: 0), from: NSRect.zero, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
+    sample.draw(at: NSPoint(x: sample.size.width * 2, y: 0), from: NSRect(x: 0, y:0, width: (1920 - sample.size.width * 2), height: 1080), operation: NSCompositingOperation.sourceOver, fraction: 1.0)
+    // sample2.draw(at: NSPoint(x: 1000, y: 0), from: NSRect.zero, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
+     // Or any of the other about 6 options; see Apple's guide to pick.
+    //anotherImage = NSImage(contentsOf: sample2)
+
+    resultImage.unlockFocus()
+    return resultImage
+}
+
+extension NSScreen{
+    static let screenWidth = NSScreen.main?.frame.width
+    static let screenHeight = NSScreen.main?.frame.height
+    static let screenSize = NSScreen.main?.frame.size
 }
 
 extension NSImage {
@@ -56,17 +85,24 @@ func resizedImage(at url: URL, for size: CGSize) -> NSImage? {
                    size: CGSize(width: size.width,height: size.height))
 }
 
-let theURL = URL(string: "file:///Users/dimeglio/Downloads/kc_nemutai-13/kc_nemutai-1371059354284093441-20210314_072317-img1.jpg")
+let dirName = "file:///Users/dimeglio/Downloads/kc_nemutai-13/"
 
-let newImage = resizedImage(at: theURL!, for: CGSize(width: 1920, height: 1080))
-let imageSize = newImage?.size.height
+//let theURL = URL(string: "file:///Users/dimeglio/Downloads/kc_nemutai-13/kc_nemutai-1400405510063411204-20210603_065426-img2.jpg")
+let theURL = URL(string: "file:///Users/dimeglio/Downloads/kc_nemutai-13/kc_nemutai-1405485960968556546-20210617_072220-img1.jpg")
 let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
-let destinationURL: URL = desktopURL.appendingPathComponent("my-image.png")
-if newImage!.pngWrite(to: destinationURL) { //, options: .withoutOverwriting) {
-            print("File saved")
-        }
-print("Hello, World!")
-print(imageSize!,  newImage?.size.width as Any)
+
+let origImage = NSImage(contentsOf: theURL!)
+let height = origImage!.size.height
+let ratio = 1080.0 / height
+let newWidth = (origImage!.size.width) * ratio
+
+let newImage = resizedImage(at: theURL!, for: CGSize(width: newWidth, height: 1080))
+let imageSize = newImage?.size.height
+let finalImage = buildWallpaper(sample: newImage!, sample2: newImage!, desktopSize: NSSize(width: 1920, height: 1080))
+let destinationURL: URL = desktopURL.appendingPathComponent("my-image23.png")
+if finalImage.pngWrite(to: destinationURL) {
+    print("File saved")
+}
 
 setBackground(theURL: (destinationURL.absoluteString))
 
