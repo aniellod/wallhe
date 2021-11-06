bafda5c5e28729dc7e0f14b8f0793a1d58766572
Aniello <aniellod@hotmail.com>
2021-11-05 20:33:34 -0400

    working with random picture

--- wallhe/main.swift
+++ wallhe/main.swift
@@ -26,16 +26,9 @@ func buildWallpaper(sample: NSImage, sample2: NSImage, desktopSize: NSSize) -> N
     //  Converted to Swift 5.5 by Swiftify v5.5.22755 - https://swiftify.com/
     let resultImage = NSImage(size: (NSMakeSize(1920,1080)))
     resultImage.lockFocus()
-    //let sample2 = sample
-    //var anotherImage = NSImage(contentsOf: sample1)
     sample.draw(at: NSPoint(x: 0, y: 0), from: NSRect.zero, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
-   
     sample.draw(at: NSPoint(x: sample.size.width, y: 0), from: NSRect.zero, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
     sample.draw(at: NSPoint(x: sample.size.width * 2, y: 0), from: NSRect(x: 0, y:0, width: (1920 - sample.size.width * 2), height: 1080), operation: NSCompositingOperation.sourceOver, fraction: 1.0)
-    // sample2.draw(at: NSPoint(x: 1000, y: 0), from: NSRect.zero, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
-     // Or any of the other about 6 options; see Apple's guide to pick.
-    //anotherImage = NSImage(contentsOf: sample2)
-
     resultImage.unlockFocus()
     return resultImage
 }
@@ -85,24 +78,44 @@ func resizedImage(at url: URL, for size: CGSize) -> NSImage? {
                    size: CGSize(width: size.width,height: size.height))
 }
 
-let dirName = "file:///Users/dimeglio/Downloads/kc_nemutai-13/"
+func updateWallpaper(path: String) {
+    let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
+    let destinationURL: URL = desktopURL.appendingPathComponent("my-image1.png")
+    let deleted = FileManager()
+    do {
+           try deleted.removeItem(at: destinationURL)
+       } catch { print(error) }
+    
+    let theURL = URL(string: path)
 
-//let theURL = URL(string: "file:///Users/dimeglio/Downloads/kc_nemutai-13/kc_nemutai-1400405510063411204-20210603_065426-img2.jpg")
-let theURL = URL(string: "file:///Users/dimeglio/Downloads/kc_nemutai-13/kc_nemutai-1405485960968556546-20210617_072220-img1.jpg")
-let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
+    let origImage = NSImage(contentsOf: theURL!)
+    let height = origImage!.size.height
+    let ratio = 1080.0 / height
+    let newWidth = (origImage!.size.width) * ratio
 
-let origImage = NSImage(contentsOf: theURL!)
-let height = origImage!.size.height
-let ratio = 1080.0 / height
-let newWidth = (origImage!.size.width) * ratio
-
-let newImage = resizedImage(at: theURL!, for: CGSize(width: newWidth, height: 1080))
-let imageSize = newImage?.size.height
-let finalImage = buildWallpaper(sample: newImage!, sample2: newImage!, desktopSize: NSSize(width: 1920, height: 1080))
-let destinationURL: URL = desktopURL.appendingPathComponent("my-image23.png")
-if finalImage.pngWrite(to: destinationURL) {
-    print("File saved")
+    let newImage = resizedImage(at: theURL!, for: CGSize(width: newWidth, height: 1080))
+    //let imageSize = newImage?.size.height
+    let finalImage = buildWallpaper(sample: newImage!, sample2: newImage!, desktopSize: NSSize(width: 1920, height: 1080))
+    
+    if finalImage.pngWrite(to: destinationURL) {
+        print("File saved")
+    }
+    setBackground(theURL: (destinationURL.absoluteString))
 }
 
-setBackground(theURL: (destinationURL.absoluteString))
+let dirName = "/Users/dimeglio/Downloads/kc_nemutai-13/"
 
+let directoryURL = URL(string: dirName)!
+let filemgr = FileManager.default
+
+do {
+    let filelist = try filemgr.contentsOfDirectory(atPath: dirName)
+
+   // for filename in filelist {
+    let theNextUrl = "file://" + dirName + filelist.randomElement()!
+        print("\(theNextUrl)")
+        updateWallpaper(path: theNextUrl)
+   // }
+} catch let error {
+    print("Error: \(error.localizedDescription)")
+}
